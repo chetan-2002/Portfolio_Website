04c2cf968de468ae7c6bb992512ed7da05253d6a
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-07-01 01:07:38 +0530

    minor code changes

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -10,81 +10,39 @@ const Project = () => {
         </header>
         <div className="grid grid-cols-1 gap-4 md: grid md:grid-cols-2 md:gap-4 md:mb-3 my-12">
           {projects.map((project, index) => (
-            <article className="" key={index}>
-              {/* <picture
-                className={
-                  index % 2 === 0
-                    ? "w-full md:w-6/12 shadow-lg "
-                    : "w-full md:w-6/12 shadow-lg md:order-1"
-                }
-              >
-                <source
-                  srcSet={`${project.image}-1280.webp 1920w,
-                        ${project.image}-768.webp 768w,
-                        ${project.image}-320.webp 320w`}
-                  sizes="(min-width: 768px) 50vw, 100vw"
-                  type="image/webp"
-                />
-                <source
-                  srcSet={`${project.image}-768.jpg 768w,`}
-                  sizes="(min-width: 768px) 50vw, 100vw"
-                  type="image/jpg"
-                />
-
-                <img
-                  width="768px"
-                  height="575px"
-                  loading="lazy"
-                  alt={project.title}
-                  className="rounded-lg"
-                />
-              </picture> */}
-              {/* <image
-                className={
-                  index % 2 === 0
-                    ? "w-full md:w-6/12 shadow-lg "
-                    : "w-full md:w-6/12 shadow-lg md:order-1"
-                }
-                src={project.image}
-                width="768px"
-                height="575px"
-                loading="lazy"
-                alt={project.title}
-              ></image> */}
-              <div className="flex flex-col overflow-auto  space-y-3 my-3 ">
-                <image src={project.image}></image>
-                <h3 className="uppercase font-bold text-lg">{project.title}</h3>
-                <p>{project.description}</p>
-                <div className="flex overflow-auto space-x-3 pb-2">
-                  {project.tools.map((disc, index) => (
-                    <span
-                      className="border border-gray-500 px-2 py-1 rounded-lg text-sm"
-                      key={index}
-                    >
-                      {disc}
-                    </span>
-                  ))}
-                </div>
-                <div className="w-auto flex space-x-5 relative">
-                  <a href={project.github} target="_blank" rel="noreferrer">
-                    <img
-                      src="./images/icons/github.svg"
-                      alt="link to github page"
-                      width="24px"
-                      height="24px"
-                    />
-                  </a>
-                  <a href={project.link} target="_blank" rel="noreferrer">
-                    <img
-                      src="./images/icons/external-link.svg"
-                      alt="link to live website"
-                      width="24px"
-                      height="24px"
-                    />
-                  </a>
-                </div>
+            <div className="flex flex-col overflow-auto  space-y-3 my-3 ">
+              <image src={project.image}></image>
+              <h3 className="uppercase font-bold text-lg">{project.title}</h3>
+              <p>{project.description}</p>
+              <div className="flex overflow-auto space-x-3 pb-2">
+                {project.tools.map((disc, index) => (
+                  <span
+                    className="border border-gray-500 px-2 py-1 rounded-lg text-sm"
+                    key={index}
+                  >
+                    {disc}
+                  </span>
+                ))}
               </div>
-            </article>
+              <div className="w-auto flex space-x-5 relative">
+                <a href={project.github} target="_blank" rel="noreferrer">
+                  <img
+                    src="./images/icons/github.svg"
+                    alt="link to github page"
+                    width="24px"
+                    height="24px"
+                  />
+                </a>
+                <a href={project.link} target="_blank" rel="noreferrer">
+                  <img
+                    src="./images/icons/external-link.svg"
+                    alt="link to live website"
+                    width="24px"
+                    height="24px"
+                  />
+                </a>
+              </div>
+            </div>
           ))}
         </div>
       </section>
