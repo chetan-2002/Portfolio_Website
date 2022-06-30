fc5277561137b01b640c02d37e519896ef2405bf
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-07-01 01:06:31 +0530

    Portfolio completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,11 +10,8 @@ function App() {
   return (
     <Suspense
       fallback={
-        <div className="sk-folding-cube">
-          <div className="sk-cube1 sk-cube"></div>
-          <div className="sk-cube2 sk-cube"></div>
-          <div className="sk-cube4 sk-cube"></div>
-          <div className="sk-cube3 sk-cube"></div>
+        <div>
+          <h1>Loading</h1>
         </div>
       }
     >
---
+++ frontend/src/projectData.js
@@ -0,0 +1,39 @@
+const projects = [
+  {
+    title: "Local Assist React App",
+    image: "./images/local_assist.png",
+    description:
+      "A Social Interactive Platform to ask and answer about everything related to your locality, build a community, and help people get aware of their neighbourhood.",
+    tools: ["React", "Redux", "JavaScript", "Firebase"],
+    github: "https://github.com/chetan-2002/Local_Assist_React_App",
+    link: "https://localassist.netlify.app/",
+  },
+  {
+    title: "Buy It",
+    image: "./images/buy_it",
+    description:
+      "A web application that displays you all the property for buy and rent in UAE (But why in UAE , Because there was no API that gets me Real Estate data for India )",
+    tools: ["React", "API", "Next", "JavaScript"],
+    github: "https://github.com/chetan-2002/BuyIT",
+    link: "https://buy-it-4dqfq74my-chetan-2002.vercel.app/",
+  },
+  {
+    title: "Tic-Tac-Toe",
+    image: "./images/local_assist.png",
+    description: "A simple tic-tac-toe game made using c++ graphics.",
+    tools: ["C++", "Graphics"],
+    github: "https://github.com/chetan-2002/Tic-Tac-Toe",
+    link: "https://github.com/chetan-2002/Tic-Tac-Toe",
+  },
+  {
+    title: "Expense Tracker Web application",
+    image: "./images/local_assist.png",
+    description:
+      "A web application with the help of which you can track your daily expenses.",
+    tools: ["React", "JavaScript"],
+    github: "https://github.com/chetan-2002/Expense_Tracker_Web_Application",
+    link: "https://github.com/chetan-2002/Expense_Tracker_Web_Application",
+  },
+];
+
+export default projects;
